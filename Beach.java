0c3598ad9c5e078862a6f9e2212ec8982295b9de
angelx34 <abritodiaz@smith.edu>
2023-04-23 15:33:43 -0400

    Start Co-authored-by: Bintu <bjalloh910@users.noreply.github.com>

---
+++ Beach.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Beach extends Location{
+    
+   
+
+
+    public Beach(String Road_Name, String Road_Adress) {
+        this.Road_Name = "Beach" ; 
+        this.Road_Adress =  "Beach Road ";
+        
+        
+    } 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+    
+    }
+
+
+
+
+}
---
+++ Player.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Player {
+   
+    Items energy_value; 
+    Location current_Location;
+    ArrayList<Items> Inventory ;
+    Scanner Puzzle_Answer; 
+
+public Player () {
+   Scanner Puzzle_Answer = new Scanner (System.in) ;
+
+}
+
+public void fight(){
+    //return something. subtract the life level between players
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+    
+    }
+}
